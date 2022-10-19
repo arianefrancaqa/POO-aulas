1d310ac9f72d16d5a9641f42920b352a5faa1e95
Ariane França <ariane.franca@uberall.com>
2022-10-18 21:00:13 -0300

    First commit aula2

---
+++ src/com/aula2/Main.java
@@ -0,0 +1,13 @@
+package com.aula2;
+
+public class Main {
+    public static void main(String[] args) {
+        String texto1 = "Olá mundo!! Sejam bem vindos";
+        System.out.println(texto1);
+
+        Integer v1 = 5;
+        Integer v2 = 32;
+
+        System.out.println("Resultado: " + (v1+v2));
+    }
+}
